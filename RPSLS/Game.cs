83a4205df85f378a6a4c9c5ec4203a0fd400f378
ABRAHAM YANG <abeyang13@gmail.com>
2019-07-31 14:58:10 -0500

    started the layout of the project

---
+++ RPSLS/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    class Game
+    {
+        // member variables (HAS A)
+
+
+        // constructor
+
+
+        // member methods (CAN DO)
+    }
+}
---
+++ RPSLS/Gesture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    class Gesture
+    {
+        // member variables (HAS A)
+
+
+        // constructor
+
+
+        // member methods (CAN DO)
+    }
+}
